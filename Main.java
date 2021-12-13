6c1d303d1c7c8ca6195d32b38879b620dd73ed4e
Darkcept <57904653+Darkcept@users.noreply.github.com>
2021-12-13 22:11:12 +0100

    good file

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
+import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
-  public static void main(String [] args)
-  {
-    Game game = new Game();
-    game.run();
-	}
+  public static void main(String [] args) throws IOException {
+    int card_ID = 2;
+    int attribute = 7;
 
+    String[] data = Files.readAllLines(Paths.get("cards_format.txt")).get(card_ID).split("\t");
+
+
+    if (attribute == 7) {
+      String[] d = data[attribute].split("\\*");
+      System.out.println ("number: " + d[0] + "\nattribute: " + d[1]);
+    } else {
+      System.out.println (data[attribute]);
+    }
+	}
 }
